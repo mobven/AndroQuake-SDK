1f3fff8ca69ec85104372e4b1e9b40ad21349421
ardakazanci-mobven <arda.kazanci@mobven.com>
2023-02-10 18:25:56 +0300

    ✨

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,86 +1,21 @@
 package com.example.sample
 
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.sample.ui.theme.SampleTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sdk_alert.DepremAlert
+import java.util.Locale
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SampleTheme {
-                Sample()
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
+        val deviceLang: Locale = resources.configuration.locales.get(0)
 
-@Composable
-fun Sample(fiyat : Int) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
-    ) {
-
-
-
-        Image(
-            painter = painterResource(id = R.drawable.spiderman),
-            contentDescription = null,
-            Modifier
-                .padding(0.dp)
-                .fillMaxWidth()
-                .height(200.dp)
+        DepremAlert.showAlertAction(
+            this,
+            deviceLang.country
         )
-
-        Column() {
-            Text(text ="En ucuz oyun $fiyat")
-            Text(text ="En ucuz oyun")
-            Text(text ="En ucuz oyun")
-            Text(text ="En ucuz oyun")
-            Text(text ="En ucuz oyun")
-            Text(text ="En ucuz oyun")
-            Text(text ="En ucuz oyun")
-
-            Image(
-                painter = painterResource(id = R.drawable.spiderman),
-                contentDescription = null,
-                Modifier
-                    .padding(0.dp)
-                    .fillMaxWidth()
-                    .height(200.dp)
-            )
-        }
-
-
-
-
-
-    }
-}
-
-@Preview(showBackground = true)
-@Preview("Article screen navrail PİXEL XL", device = Devices.PIXEL_XL)
-@Composable
-fun DefaultPreview() {
-    SampleTheme {
-
     }
 }
